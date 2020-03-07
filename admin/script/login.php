558a9f3a6c1dcef6bc2f5c6dc4897c55eb6dcfcf
ling89889988952 <zouling707@gmail.com>
2020-03-07 14:34:38 -0500

    add the function of admin login

--- admin/script/login.php
+++ admin/script/login.php
@@ -1,7 +1,7 @@
 <?php
 
 function login($username, $password){
-
+    
     $pdo = Database::getInstance()->getConnection();
     // check user existance
     $check_exist_query = 'SELECT COUNT(*) FROM tbl_admin WHERE username = :username';
@@ -14,7 +14,6 @@ function login($username, $password){
 
     
     if ($user_set->fetchColumn()>0){
-        // If the password did not encrypt, using the method to check the username and password
         $get_user_query = 'SELECT * FROM tbl_admin WHERE username = :username';
         $get_user_query .= ' AND password = :password';
         $user_check = $pdo->prepare($get_user_query);
@@ -25,6 +24,7 @@ function login($username, $password){
             )
             );   
 
+
             while($found_user = $user_check->fetch(PDO::FETCH_ASSOC)){
                 $id = $found_user['admin_id'];
                 // logged in
@@ -32,20 +32,71 @@ function login($username, $password){
                 $_SESSION['username'] = $found_user['username'];
 
             }
- 
-
+            
             if(isset($id)){
                 $message = 'You just logged in !';
                 // login to a welcome page
-                redirect_to('admin.php');
+                redirect_to('admin_edituser.php');
 
             }else{      
+                
+                $query_password = 'SELECT * FROM tbl_admin WHERE username = :username';
+                $password_get= $pdo->prepare($query_password );
+                $password_get->execute(
+                array(
+                 ':username'=>$username,
+                )
+                );   
+                
+                $user_arry = $password_get->fetch(PDO::FETCH_ASSOC);
+                $count = $user_arry['login_count'];
+               
+                if($count == 1){
+                    $login_time = $user_arry['login_time'];
+
+                    // get the now time
+                    date_default_timezone_set("America/Toronto");
+                    $now_date = date("Y-m-d H:i:s");
+                    // change the now time to strtotime and set the last time = after 12 hour
+                    $now = strtotime($now_date);
+
+                    $last = strtotime("$login_time + 12 hours");
                     
+                    // compare the now time data <  the data of (late time + 12hours)
+                    if($now > $last){
+                        $message = 'Your account has been suspended.';
+                        session_destroy();
+                    }else{
+                        $password_hash = $user_arry['password'];
+                        // verify the password(post) and the user_password(from database)
+                    if(password_verify($password, $password_hash)){
+                        $message = "Your password is right";
+                        $user_id = $user_arry['admin_id'];
+                        $_SESSION['admin_id'] = $user_id ;
+                        $_SESSION['username'] = $user_arry['username'];
+
+                    redirect_to('admin.php');
+                        }else{
+                    $message ='wrong password!';
+                }
+            }
+        }else{
+               $password_hash = $user_arry['password'];
+                // verify the password(post) and the user_password(from database)
+                if(password_verify($password, $password_hash)){
+                $message = "Your password is right";
+                $user_id = $user_arry['admin_id'];
+                $_SESSION['admin_id'] = $user_id ;
+                $_SESSION['username'] = $user_arry['username'];
+                redirect_to('admin.php');
+                }else{     
                 $message ='wrong password!';
+                }
+            }           
+                
             }
   }else {
-        $message ='User does not exist!';
-    }
+        $message ='User does not exist!'; }
 
     return $message;  
      
